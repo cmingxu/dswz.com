ad13c6bd42586863fa19ddf73e12be41f761f6a5
Kevin.Xu <cmxu@kabam.com>
2014-10-14 23:51:06 +0800

    deploy config change

--- config/dswz.sh
+++ config/dswz.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 # rvm wrapper ruby-1.9.3-p194 bootup
 UNICORN=/home/ubuntu/.rvm/bin/bootup_unicorn
-CONFIG_FILE=/home/ubuntu/codes/current/config/unicorn.rb
-APP_HOME=/home/ubuntu/codes/current
+CONFIG_FILE=/home/ubuntu/dswz/current/config/unicorn.rb
+APP_HOME=/home/ubuntu/dswz/current
  
 case "$1" in
   start)
   $UNICORN -c $CONFIG_FILE -E production -D
   ;;
   stop)
-  kill -QUIT `cat /tmp/unicorn.pid`
+  kill -QUIT `cat /tmp/unicorn_dswz.pid`
   ;;
   restart|force-reload)
-    kill -USR2 `cat /tmp/unicorn.pid`
+    kill -USR2 `cat /tmp/unicorn_dswz.pid`
   ;;
   *)
    echo "Usage: $SCRIPTNAME {start|stop|restart|force-reload}" >&2
