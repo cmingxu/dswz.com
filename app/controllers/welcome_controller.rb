b45d66505f1d99a4b579d16ec4fd504b60c70c83
Kevin.Xu <cmxu@kabam.com>
2014-10-17 22:28:04 +0800

    page done

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+
+
+  ABOUT_PAGES.keys.each do |p|
+
+    define_method p do
+      
+      render "page"
+    end
+
+  end
 end
